14e12b2d3bc8d137a40b51849586a3412f074d0d
Jordan Phillips <jordan@bonanza.com>
2011-10-03 17:29:42 -0700

    Update bilateral smoothing method to send all four args to cvSmooth

--- ext/opencv/cvmat.cpp
+++ ext/opencv/cvmat.cpp
@@ -4157,11 +4157,11 @@ rb_smooth_median(int argc, VALUE *argv, VALUE self)
 VALUE
 rb_smooth_bilateral(int argc, VALUE *argv, VALUE self)
 {
-  VALUE p1, p3, p4, dest;
-  rb_scan_args(argc, argv, "03", &p1, &p3, &p4);
+  VALUE p1, p2, p3, p4, dest;
+  rb_scan_args(argc, argv, "04", &p1, &p2, &p3, &p4);
   CvArr* self_ptr = CVARR(self);
   dest = new_mat_kind_object(cvGetSize(self_ptr), self);
-  cvSmooth(self_ptr, CVARR(dest), CV_BILATERAL, IF_INT(p1, 0), 0, IF_INT(p3, 3), IF_INT(p4, 3));
+  cvSmooth(self_ptr, CVARR(dest), CV_BILATERAL, IF_INT(p1, 0), IF_INT(p2, 0), IF_INT(p3, 3), IF_INT(p4, 3));
   return dest;
 }
 
@@ -4192,7 +4192,7 @@ rb_smooth(int argc, VALUE *argv, VALUE self)
     break;
   case CV_BILATERAL:
     smooth_func = rb_smooth_bilateral;
-    argc = (argc > 3) ? 3 : argc;
+    argc = (argc > 4) ? 4 : argc;
     break;
   default:
     smooth_func = rb_smooth_gaussian;
