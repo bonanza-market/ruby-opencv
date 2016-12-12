d1c8fc31b771aba94f72bf9082aa9b3fb55a04f5
Alex Fawkes <alexf@bonanza.com>
2016-12-12 11:56:07 -0800

    Remove unnecessary layer

--- lib/opencv/cvmat.rb
+++ lib/opencv/cvmat.rb
@@ -1,7 +1,13 @@
-require "bonz/mat_mixin"
+require "bonz/mat_measure_mixin"
+require "bonz/mat_border_mixin"
+require "bonz/mat_mask_mixin"
+require "bonz/mat_general_mixin"
 
 module OpenCV
   class CvMat
-    include Bonz::MatMixin
+    include Bonz::MatMeasureMixin
+    include Bonz::MatBorderMixin
+    include Bonz::MatMaskMixin
+    include Bonz::MatGeneralMixin
   end
 end
