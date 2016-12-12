b52986db058367d62aa51214a1ea9dbfbc0d46e6
Alex Fawkes <alexf@bonanza.com>
2016-12-12 11:53:19 -0800

    Pass tests

--- lib/bonz/mat_mixin.rb
+++ lib/bonz/mat_mixin.rb
@@ -1,5 +1,4 @@
 require "bonz/mat_measure_mixin"
-require "bonz/mat_trace_mixin"
 require "bonz/mat_border_mixin"
 require "bonz/mat_mask_mixin"
 require "bonz/mat_general_mixin"
@@ -9,7 +8,7 @@ module Bonz
     extend self
 
     def included(mod)
-      mixins = [ MatMeasureMixin, MatTraceMixin, MatBorderMixin, MatMaskMixin, MatGeneralMixin ]
+      mixins = [ MatMeasureMixin, MatBorderMixin, MatMaskMixin, MatGeneralMixin ]
       mixins.each do |m|
         mod.include(m::InstanceMethods)
         mod.extend(m::ClassMethods)
